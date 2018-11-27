fc9b4acdbf1ee3fba47c5d3db673a16155a2e8f4
matt <matthew.robbins@gmail.com>
2018-11-27 10:47:40 -0500

    Initial Commit

---
+++ VisualCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualCalc
+//
+//  Created by Matthew E Robbins on 11/27/18.
+//  Copyright © 2018 Matthew E Robbins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
